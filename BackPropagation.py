f0535ae9ebd99ec00a27e09949395ea96dc33060
michael-poole <37667597+michael-poole@users.noreply.github.com>
2018-10-01 11:53:23 -0500

    Completed Project
    Finished the neural network, allowing it to train and test on data sets of varying sizes.

--- BackPropagation.py
+++ BackPropagation.py
@@ -1,6 +1,8 @@
 from random import seed
+from random import randrange
 from random import random
 from datetime import datetime
+from csv import reader
 from math import exp
 
 
@@ -56,6 +58,14 @@ class Neuron:
     def calculate_output_error(self, neuron_index, expected_outputs):
         return expected_outputs[neuron_index] - self.output
 
+    def accumulate_inputs(self):
+        return self.output
+
+    def update_neuron_weights(self, inputs, l_rate):
+        for j in range(len(inputs)):
+            self.weights[j] += l_rate * self.delta * inputs[j]
+        self.weights[-1] += l_rate * self.delta
+
 
 class NeuralNetwork:
     def __init__(self, input_nodes, hidden_nodes, output_nodes):
@@ -105,7 +115,7 @@ class NeuralNetwork:
             inputs = new_inputs
         return inputs
 
-    def backward_propagate_error(self, expected):
+    def backward_propagate_error(self, expected_outputs):
         # Start with the output and work backwards (Must be reversed)
         for i in reversed(range(len(self.network))):
             layer = self.network[i]
@@ -124,18 +134,172 @@ class NeuralNetwork:
                 # For each neuron, the error is the expected output minus the actual output
                 for j in range(len(layer)):
                     neuron = layer[j]
-                    errors.append(neuron.calculate_output_error(j, expected))
+                    errors.append(neuron.calculate_output_error(j, expected_outputs))
             # For each neuron, calculate the
             for j in range(len(layer)):
                 neuron = layer[j]
                 neuron.calculate_delta_error(errors[j])
 
+    def update_network_weights(self, row, l_rate):
+        for i in range(len(self.network)):
+            inputs = row[:-1]
+            if i != 0:
+                inputs = []
+                for neuron in self.network[i-1]:
+                    inputs.append(neuron.accumulate_inputs())
+            for neuron in self.network[i]:
+                neuron.update_neuron_weights(inputs, l_rate)
 
-seed(1)  # Makes sure the random numbers always start the same.
-# seed(datetime.now())  # Makes sure the random numbers are different each time we start
-myNetwork = NeuralNetwork(2, 3, 2)
-myNetwork.create_network()
-output = myNetwork.forward_propagate([1, 0])
-expected = [0, 1]
-myNetwork.backward_propagate_error(expected)
-myNetwork.print_network()
+    def train(self, data, learn_rate, n_epochs, n_outputs):
+        # train a certain number of times (epochs)
+        for epoch in range(n_epochs):
+            sum_error = 0
+            # For each set of data
+            for row in data:
+                # Do the first run through, get the first output
+                outputs = self.forward_propagate(row)
+                # Initializes the outputs to all equal 0
+                expected_outputs = [0 for i in range(n_outputs)]
+                # Sets each of these outputs to its actual value
+                expected_outputs[row[-1]] = 1
+                sum_error += sum([(expected_outputs[i] - outputs[i]) ** 2 for i in range(len(expected_outputs))])
+                self.backward_propagate_error(expected_outputs)
+                self.update_network_weights(row, learn_rate)
+            print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch + 1, learn_rate, sum_error))
+
+    def predict(self, row):
+        outputs = self.forward_propagate(row)
+        prediction = outputs.index(max(outputs))
+        # print('Expected=%d, Got=%d' % (row[-1], prediction))
+        return prediction
+
+
+def load_csv(my_file):
+    data = list()
+    with open(my_file, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            data.append(row)
+    return data
+
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+def dataset_minmax(dataset):
+    stats = [[min(column), max(column)] for column in zip(*dataset)]
+    return stats
+
+
+def normalize_dataset(dataset, minmax):
+    for row in dataset:
+        for i in range(len(row)-1):
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+
+def parse_csv(filename):
+    dataset = load_csv(filename)
+    for i in range(len(dataset[0]) - 1):
+        str_column_to_float(dataset, i)
+    # convert class column to integers
+    str_column_to_int(dataset, len(dataset[0]) - 1)
+    # normalize input variables
+    minmax = dataset_minmax(dataset)
+    normalize_dataset(dataset, minmax)
+    return dataset
+
+
+def backprop_algorithm(train, test, l_rate, n_epoch, n_hidden):
+    n_inputs = len(train[0]) - 1
+    n_outputs = len(set([row[-1] for row in train]))
+    my_network = NeuralNetwork(n_inputs, n_hidden, n_outputs)
+    my_network.create_network()
+
+    my_network.train(train, l_rate, n_epoch, n_outputs)
+    predictions = list()
+    for row in test:
+        prediction = my_network.predict(row)
+        predictions.append(prediction)
+    return predictions
+
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+def run_algorithm(fold, train_set, test_set, learning_rate,
+                  number_of_epochs, number_of_hidden_nodes):
+    predicted = backprop_algorithm(train_set, test_set, learning_rate,
+                                   number_of_epochs, number_of_hidden_nodes)
+    actual = [row[-1] for row in fold]
+    accuracy = accuracy_metric(actual, predicted)
+    score = (accuracy)
+    return score
+
+
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+
+def initialize_algorithm(data, number_of_folds, learning_rate, number_of_epochs, number_of_hidden_nodes):
+    folds = cross_validation_split(data, number_of_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted_result = run_algorithm(fold, train_set, test_set, learning_rate,
+                                         number_of_epochs, number_of_hidden_nodes)
+        scores.append(predicted_result)
+    return scores
+
+
+def main():
+    seed(1)  # Makes sure the random numbers always start the same
+    # seed(datetime)  # Makes sure the random numbers always start differently.
+    my_dataset = parse_csv('iris.csv')
+    number_of_folds = 5
+    learning_rate = 0.3
+    number_of_epochs = 500
+    number_of_hidden_nodes = 5
+
+    my_scores = initialize_algorithm(my_dataset, number_of_folds, learning_rate, number_of_epochs,
+                                     number_of_hidden_nodes)
+    print('Scores: %s' % my_scores)
+    print('Mean Accuracy: %.3f%%' % (sum(my_scores) / float(len(my_scores))))
+
+
+main()
